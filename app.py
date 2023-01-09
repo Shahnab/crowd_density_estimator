9a111ad84285ca4fc7eead03fd02bc16bbef3fdc
SHAHNAB AHMED <31092371+Shahnab@users.noreply.github.com>
2023-01-09 11:44:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st. set_page_config(layout="wide")
 
 st.sidebar.image("https://www.microlog.it/wp-content/uploads/2018/11/icona_soluz_contapersone_col.png", width=100)
 st.sidebar.subheader("About Crowd Counting Model")
-st.sidebar.markdown("##### An end-to-end trainable deep architecture that combines features obtained using multiple receptive field sizes and learns the importance of each such feature at each image location")
-st.sidebar.markdown("##### The approach adaptively encodes the scale of the contextual information required to accurately predict crowd density")
-st.sidebar.markdown("##### This yields an algorithm that outperforms state-of-the-art crowd counting methods, especially when perspective effects are strong")
+st.sidebar.markdown("###### An end-to-end trainable deep architecture that combines features obtained using multiple receptive field sizes and learns the importance of each such feature at each image location")
+st.sidebar.markdown("###### The approach adaptively encodes the scale of the contextual information required to accurately predict crowd density")
+st.sidebar.markdown("###### This yields an algorithm that outperforms state-of-the-art crowd counting methods, especially when perspective effects are strong")
 
 st.sidebar.subheader("Context-Aware Network Model")
 st.sidebar.image("model.png")
